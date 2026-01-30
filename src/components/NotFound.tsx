a9899df0edfdad399cfd2b98ebef380a5718a1bc
freethedev <freethedev@proton.me>
2026-01-30 20:39:14 +0100

    Fix the 404 page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { Button, Result } from "antd";
 
+import { getErrorMessage } from "../utils";
+
 const NotFound: React.FunctionComponent = () => {
     return (
         <Result
             status={404}
-            subTitle={<Button href="/">Back to homepage</Button>}
+            title={getErrorMessage(404)}
+            subTitle={<Button type="primary" href="/">Back to homepage</Button>}
         />
     );
 };
